cccbba0e9df4da8916dda1c8e8901ef305d8ee0b
Benjaminwills <benjaminwills047@gmail.com>
2023-01-02 23:34:43 +0000

    created scraper class

--- web_scraper/scraper/scraper.py
+++ web_scraper/scraper/scraper.py
@@ -1,8 +1,23 @@
-import bs4 as beautiful_soup
 import requests
+
+from bs4 import BeautifulSoup
 
 page = requests.get(
     "https://uk.indeed.com/jobs?q=&l=Berkhamsted%2C+Hertfordshire&from=searchOnHP&vjk=edc330f2dc5f7f73"
 )
 
-print(page.status_code)
+
+class Scraper:
+    def __init__(self, url: str) -> None:
+        self.url = url
+
+        # CREATING THE PARSER
+        self.soup = self.__get_beautifulsoup_parser()
+
+    def __get_page_info(self) -> bytes:
+        page = requests.get(self.url)
+        return page.content
+
+    def __get_beautifulsoup_parser(self):
+        parser = BeautifulSoup(self.__get_page_info(), "html.parser")
+        return parser
