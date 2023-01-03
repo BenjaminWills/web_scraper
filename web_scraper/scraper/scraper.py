8ab58642655f1c3b96c1d04f97d665b5d751b128
Benjaminwills <benjaminwills047@gmail.com>
2023-01-03 09:40:37 +0000

    added find jobs method

--- web_scraper/scraper/scraper.py
+++ web_scraper/scraper/scraper.py
@@ -4,6 +4,10 @@ from bs4 import BeautifulSoup
 
 
 class Scraper:
+    """
+    A scraper for reed.com
+    """
+
     def __init__(self, url: str) -> None:
         self.url = url
 
@@ -17,3 +21,7 @@ class Scraper:
     def __get_beautifulsoup_parser(self) -> BeautifulSoup:
         parser = BeautifulSoup(self.__get_page_info(), "html.parser")
         return parser
+
+    def find_jobs(self, tag: str, css_class: str) -> list:
+        jobs = self.parser.find_all(tag, class_=css_class)
+        return jobs
