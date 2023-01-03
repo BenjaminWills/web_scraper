6dbfa2e0017a23cd6e8aa9a090a6af6377cd1d2e
Benjaminwills <benjaminwills047@gmail.com>
2023-01-03 10:39:48 +0000

    functional parser

--- web_scraper/scraper/scraper.py
+++ web_scraper/scraper/scraper.py
@@ -28,33 +28,33 @@ class Scraper:
         jobs = self.parser.find_all(tag, class_=css_class)
         return jobs
 
-    def __parse_salary(self,salary_tag):
+    def __parse_salary(self, salary_tag):
         salary = salary_tag.text.strip()
         return salary
 
-    def __parse_county(self,location_tag):
+    def __parse_county(self, location_tag):
         county_tag = location_tag.find("span")
         county = county_tag.text.strip()
         return county
 
-    def __parse_location(self,location_tag):
-        location_tag_text = location_tag.textself.
+    def __parse_location(self, location_tag):
+        location_tag_text = location_tag.text
         split_newline = location_tag_text.split("\n")
         location = split_newline[1].strip()
         return location
 
-    def __parse_position(self,position_tag):
+    def __parse_position(self, position_tag):
         return position_tag.text.strip()
 
-    def __parse_job_title(self,job):
+    def __parse_job_title(self, job):
         title_tag = job.find("h2")
         return title_tag.text.strip()
 
-    def __parse_job_description(self,job):
-        job_description = job.find('p',{"class":"job-result-description__details"})
+    def __parse_job_description(self, job):
+        job_description = job.find("p", {"class": "job-result-description__details"})
         return job_description.text.strip()
 
-    def parse_job(self,job: element.Tag) -> dict:
+    def parse_job(self, job: element.Tag) -> dict:
         list_tags = job.find_all("li")
         salary_tag, location_tag, position_tag = (
             list_tags[0],
@@ -74,9 +74,9 @@ class Scraper:
         except Exception as e:
             return None
 
-    def parse_jobs(self,jobs: List[element.Tag]) -> List[dict]:
-        job_dictionaries = [
-            self.parse_job(job) 
-            for job in jobs
-        ]
+    def parse_jobs(
+        self,
+    ) -> List[dict]:
+        jobs = self.find_jobs("div", "col-sm-12 col-md-9 details")
+        job_dictionaries = [self.parse_job(job) for job in jobs]
         return job_dictionaries
