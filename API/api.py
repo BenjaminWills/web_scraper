9a1151e071317f7c8224490bfb4b69a02b255d4f
Benjaminwills <benjaminwills047@gmail.com>
2023-01-04 15:43:00 +0000

    api progress

--- API/api.py
+++ API/api.py
@@ -1,8 +1,27 @@
-from flask import Flask
+import os
 
-app = Flask(__name__)
+from dotenv import load_dotenv
+from flask import Flask, request
+
+app = Flask(__name__, static_folder="web_scraper")
+
+# from web_scraper.database.sql_wrapper import Sql
+
+load_dotenv(override=True)
 
 
-@app.route("/")
-def test():
-    return "<h1>Hello, World!</h1>"
+api_key = os.getenv("api_key")
+
+user = os.getenv("user")
+password = os.getenv("password")
+host = os.getenv("host")
+port = os.getenv("port")
+database = os.getenv("database")
+
+# sql = Sql(user, password, host, port, database)
+
+
+@app.route("/find/job-by-id/<int:id>", methods=["GET"])
+def find_job_by_id(id: int):
+    if request.method == "GET":
+        return f"{id}"
