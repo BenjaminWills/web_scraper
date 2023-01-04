3b0b589a748cbb26910b2c9fb4aa335d5cce8791
Benjaminwills <benjaminwills047@gmail.com>
2023-01-04 16:27:06 +0000

    fixed import issue

--- API/api.py
+++ API/api.py
@@ -3,9 +3,9 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, request
 
-app = Flask(__name__, static_folder="web_scraper")
+app = Flask(__name__)
 
-# from web_scraper.database.sql_wrapper import Sql
+from web_scraper.database.sql_wrapper import Sql
 
 load_dotenv(override=True)
 
@@ -18,7 +18,7 @@ host = os.getenv("host")
 port = os.getenv("port")
 database = os.getenv("database")
 
-# sql = Sql(user, password, host, port, database)
+sql = Sql(user, password, host, port, database)
 
 
 @app.route("/find/job-by-id/<int:id>", methods=["GET"])
