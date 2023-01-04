1752600a6a1af8784fdb2b2ef8973945bcf3cba0
Benjaminwills <benjaminwills047@gmail.com>
2023-01-04 17:04:10 +0000

    added json outputs

--- API/api.py
+++ API/api.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from flask import Flask, request
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
@@ -21,20 +21,6 @@ database = os.getenv("database")
 sql = Sql(user, password, host, port, database)
 
 
-@app.route("/find/job-by-id/<int:id>", methods=["GET"])
-def find_job_by_id(id: int):
-    if request.method == "GET":
-        result = sql.execute_query(
-            f"""
-            SELECT * 
-            FROM jobs 
-            WHERE id = {id};
-            """
-        )
-
-        return job_tuple_to_dict(tuple(result[0]))
-
-
 def job_tuple_to_dict(job_tuple: tuple) -> dict:
     keys = (
         "id",
@@ -47,3 +33,29 @@ def job_tuple_to_dict(job_tuple: tuple) -> dict:
     )
     dictionary_contents = zip(keys, job_tuple)
     return dict(dictionary_contents)
+
+
+@app.route("/find/job", methods=["GET"])
+def find_job():
+    if request.method == "GET":
+        result = sql.execute_query(
+            f"""
+            SELECT * 
+            FROM jobs;
+            """
+        )
+        return jsonify([job_tuple_to_dict(job) for job in result])
+
+
+@app.route("/find/job/by-id/<int:id>", methods=["GET"])
+def find_job_by_id(id: int):
+    if request.method == "GET":
+        result = sql.execute_query(
+            f"""
+            SELECT * 
+            FROM jobs 
+            WHERE id = {id};
+            """
+        )
+
+        return jsonify(job_tuple_to_dict(tuple(result[0])))
