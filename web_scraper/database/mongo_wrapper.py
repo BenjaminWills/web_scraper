4caae7ce5610afb7b7fa0f39d86e1ff5e4af760a
Benjaminwills <benjaminwills047@gmail.com>
2023-01-11 18:24:37 +0000

    add logger

--- web_scraper/database/mongo_wrapper.py
+++ web_scraper/database/mongo_wrapper.py
@@ -1,10 +1,33 @@
 import pymongo
 
+from web_scraper.loggers.make_logger import get_logger, make_logging_directory
+
 
 class Mongo:
     def __init__(self, username: str, password: str, host: str, port: str) -> None:
-        self.conn_string = f"mongodb://{username}:{password}@{host}:{port}"
+        self.connection_string = f"mongodb://{username}:{password}@{host}:{port}"
         self.client = self.get_client()
 
+        # CREATING THE LOGGER
+        make_logging_directory()
+        self.logger = get_logger(
+            output_path="logging/mongo.log",
+            save_log=True,
+            logger_name="Mongo",
+        )
+
     def get_client(self) -> pymongo.MongoClient:
-        return pymongo.MongoClient(self.conn_string)
+        try:
+            client = pymongo.MongoClient(self.connection_string)
+            return client
+        except pymongo.errors.PyMongoError as pme:
+            self.logger.exception(pme)
+            return None
+
+    def insert_entry(self, db_name: str, collection: str, entry: dict) -> bool:
+        try:
+            with self.client as client:
+                client.close()
+        except pymongo.errors.PyMongoError as pme:
+            self.logger.exception(pme)
+            return False
